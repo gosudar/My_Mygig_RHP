43fa1e550d99ef97804510ed38fdb60eea0f214d
gosudar <gosudar1@narod.ru>
2025-03-30 22:01:59 +0300

    Update my_jeep_compass_utility.ino
    version 1.4.5
    
    added autoheadlights HVAC or LRSM

--- my_jeep_compass_utility.ino
+++ my_jeep_compass_utility.ino
@@ -1,15 +1,17 @@
 /******************************************************************************************
  * Project    : MY Jeep Compass Utility/MyGIG RHP
  * Hack for my Jeep Compass MyGIG RHP and Utility
- * Version 1.4
+ * * Version 1.4.5
  * Features:
  *  - Emulating VES presense to enable VIDEO AUX IN in MyGIG head unit
  *  - Enable intelligent cornering light
  *  - Enable digital output when pressing the steering wheel button
  *  - Enable digital output when pressing fobik Trunk button
  *  - Enable digital output when pressing fobik Lock button
+ *  - Reset counter factory Remote Start (manual)
+ *  - Autoheadlight: autoselect HVAC or LRSM
  *
- * Copyright (C) 2024 DMITRY GOSUDAR <gosudar1@narod.ru>
+ * Copyright (C) 2025 DMITRY GOSUDAR <gosudar1@narod.ru>
  * http://gosudar.org.ru
  *
  * This is free software. You may use/redistribute it under The MIT License terms.
@@ -24,46 +26,37 @@
 
 #include <CAN.h>
 
-uint8_t keyState = 0x00;                  //initial state = key-in, accessory on
-uint8_t Trunk_reset_counter_az = 0x00;       //reset az with fobik trunk button 00 - disable, 01 - enable
+uint8_t keyState = 0x00;//initial state = no-key, all-off
 
-String SerialRXBuffer = "";
-bool SerialRXSpecial = false;
-bool Engine_Run = false;// Статус двигателя true - run, false - off
+bool Engine_Run = false;// Engine Status true - run, false - off
 bool FrontFogON = false;// Включены ли туманки? true-on, false-off
 bool EnableTempFog = 0;//Временная переменная для отслеживания статуса Enable Fog
 bool RightFog = false;// Включать правую туманку? true-on, false-off
 bool LeftFog = false;// Включать левую туманку? true-on, false-off
 bool Steering_Wheel_1_flag = false;//флаг длительного нажатия true-on, false-off
-bool RKE_Trunk_Button_flag = false;//флаг нажатия кнопки багажника true-on, false-off
-bool RKE_Alarm_ON_flag = false;//флаг нажатия кнопки постановки на охрану true-on, false-off
-bool Remote_start;//Remote start true-on, false-off
-bool Jeep_Alarm_Status;//Статус охраны true-on, false-off
+bool RKE_Trunk_Button_flag = false;
+bool RKE_Alarm_ON_flag = false;
+bool Remote_start = false;//Remote start true-on, false-off
+bool Alarm_Status = false;//Alarm status true-on, false-off
+bool CAN_LOGS = true;
+bool is_dodge = true; // Dodge Caliber - no HVAC, search HVAC 
 
 int EnableFogLeft = 7; // Инициализация переменной EnableFogLeft к выводу 7
 int EnableFogRight = 6;// Инициализация переменной EnableFogRight к выводу 6
 int RKE_Alarm_ON = 5;// Инициализация переменной Alarm_ON к выводу 5
 int Steering_Wheel_1 = 4;// Инициализация переменной Steering_Wheel_1 к выводу 4
 int RKE_Trunk_Button = 3;// Инициализация переменной Steering_Wheel_1 к выводу 3
-
 int Temp_Button_SW1 = 0;//счетчик удержания левой центральной подрулевой кнопки
-int reset_az_stage = 0;//счетчик кол-ва remote start
+int reset_az_stage = 0;//счетчик кол-ва АЗ
 
-int Jeep_RPM = 0;// обороты двигателя
-int Jeep_Speed = 0;// скорость авто
-uint8_t Jeep_Gear = 0;// статус передачи вариатора (акпп)
+//int Jeep_RPM = 0;// обороты
+int Jeep_Speed = 0;// скорость
+//uint8_t Jeep_Gear = 0;// селектор коробки
 
-float Jeep_Temp_Outdoor = 0;// температура за бортом
-float Jeep_Batt;// бортовое напряжение
+float Jeep_Temp_Outdoor;// температура за бортом
+float Jeep_Batt;// напряжение
 
 uint32_t my_reset_az;
-
-//Define subroutines
-//void Enable_VES();
-void Check_FOG();
-void Check_Steering_Wheel();
-void Check_RKE_Button();
-void Check_Counter_AZ();
 
 void setup()
 { 
@@ -89,41 +82,65 @@ void setup()
  
   if (!CAN.begin(83E3))      //start the CAN bus at 83.333 kbps 
   {
-    Serial.println("Error! Starting CAN0 failed!");
+    Serial.println("Starting CAN0 failed!");
     while (1);
   }
 
   CAN.onReceive(onCANReceive);
-  Serial.println("MY Jeep Compass utility v 1.4 start:");
+  Serial.println("MY Jeep Compass utility start.");
 }
 
 void loop()
 {
-  // checkSerial() for testing
   for (uint16_t y = 0; y < 900; y++)  //~900mS delay while checking serial.
   {
     delay(1);
     checkSerial();
   }
 
-   //Enable_VES();          // Enable VES - moved to Check_Steering_Wheel
-   Check_FOG();             // Check FOG 
-   Check_Steering_Wheel();  // Check Steering Wheel buttons
-   Check_RKE_Button();      // Check RKE fobic buttons
-   Check_Counter_AZ();      // Check counter Remote start
-   //delay(30);
+  //Enable_VES();          // Enable VES - moved to Check_Steering_Wheel
+  Check_FOG();             // Check FOG 
+  Check_Steering_Wheel();  // Check Steering Wheel buttons
+  Check_RKE_Button();      // Check RKE fobic buttons
+  Check_Counter_AZ();      // Check counter AZ
+  delay(30);
+
+  // view Status
+  if (Remote_start == true)
+  {
+    Serial.println("---Remote_start =  ON ---");
+  }
+  else
+  {
+    Serial.println("---Remote_start =  OFF ---");
+  }
+  if (Engine_Run == true)
+  {
+    Serial.println("---Engine_Run =  ON ---");
+  }
+  else
+  {
+    Serial.println("---Engine_Run =  OFF ---");
+  }
+  if (Alarm_Status == true)
+  {
+    Serial.println("---Alarm Status =  ON ---");
+  }
+  else
+  {
+    Serial.println("---Alarm Status =  OFF ---");
+  }
+  Serial.print("---Jeep_Temp_Outdoor = ");Serial.print(Jeep_Temp_Outdoor);Serial.print("----"); Serial.println();
+  Serial.print("---Jeep_Batt = ");Serial.print(Jeep_Batt);Serial.print("----"); Serial.println();
+  Serial.print("---Counter Remote Start = ");Serial.print(reset_az_stage);Serial.print("----"); Serial.println();
+  delay(30);  
 }
 
 void Enable_VES()
 {
-  if ( keyState == 0x00 )
-    delay(30);
-  else
-  {
-    // VES Lockpic for Mygig
-    canSend(0x322, 0x01, 0x70, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00); delay(25); //Ves configuration
-    canSend(0x3B4, 0x00, 0x04, 0x00, 0x07, 0x00, 0x00, 0x00, 0x07); delay(25); //Ves AUX VIDEO
-  }
+  // VES Lockpic
+  canSend(0x322, 0x01, 0x70, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00); delay(25); //Ves configuration
+  canSend(0x3B4, 0x00, 0x04, 0x00, 0x07, 0x00, 0x00, 0x00, 0x07); delay(25); //Ves AUX VIDEO
 }
 
 void Check_RKE_Button()
@@ -131,31 +148,31 @@ void Check_RKE_Button()
   //RKE_Trunk_Button
   if (RKE_Trunk_Button_flag == true)
   {
-    if ( Trunk_reset_counter_az == 0x01 )
-    {
-      reset_counter_az();// to do: reset counter remote start
-    }
+    reset_counter_az();// to do: reset counter remote start
+    
+    // Fobik Key Enabled
     if (digitalRead(RKE_Trunk_Button) == 0)
     { 
-      Serial.println("---Fobik Key Trunk Enabled = OUT ON ---");
+      Serial.println("---Fobik Key Enabled = OUT ON ---");
       digitalWrite(RKE_Trunk_Button, HIGH);
       delay(5);
     }
     else
     {
-      Serial.println("---Fobik Key Trunk Enabled = OUT OFF ---");
+      Serial.println("---Fobik Key Enabled = OUT OFF ---");
       digitalWrite(RKE_Trunk_Button, LOW);
       delay(5);
     }
     RKE_Trunk_Button_flag = false;
   }
+  
   //RKE_Alarm_ON
   if (RKE_Alarm_ON_flag == true)
   {
-    // Fobik Key Enabled Lock
+    // Fobik Key Enabled Alarm ON
     if (digitalRead(RKE_Alarm_ON) == 0)
     { 
-      Serial.println("---Fobik Key Lock Enabled = Lock Alarm ON ---");
+      Serial.println("---Fobik Key Enabled = Alarm ON ---");
       digitalWrite(RKE_Alarm_ON, HIGH);
       delay(500);
       digitalWrite(RKE_Alarm_ON, LOW);
@@ -166,13 +183,10 @@ void Check_RKE_Button()
 
 void Check_Steering_Wheel()
 {
-  //Serial.println("---SW ---");
   if (Steering_Wheel_1_flag == true)
   {
     Temp_Button_SW1 = 0;//сброс счетчика нажатий
-
-    Enable_VES();  // Enable VES
-       
+    Enable_VES();  // Enable VES  
     if (digitalRead(Steering_Wheel_1) == 0)
     { 
       Serial.println("---Steering Wheel Key Enabled = OUT ON ---");
@@ -191,59 +205,61 @@ void Check_Steering_Wheel()
 
 void Check_FOG()
 {
-  if (FrontFogON == false)
+  if (Engine_Run == true)
   {
-    // Front Fog disable
-    if ( RightFog == true )
+    if (FrontFogON == false)
     {
-      //Fog right enable
-      EnableTempFog = true;
-      digitalWrite(EnableFogRight, HIGH);
-      Serial.println("---Right Fog ON---"); 
+      // Front Fog disable
+      if ( RightFog == true )
+      {
+        //Fog right enable
+        EnableTempFog = true;
+        digitalWrite(EnableFogRight, HIGH);
+        Serial.println("---Right Fog ON---"); 
+      }
+      else
+      {
+        //Fog right disable
+        EnableTempFog = false;
+        if (digitalRead(EnableFogRight) != 0)
+        { 
+          digitalWrite(EnableFogRight, LOW);
+          delay(5);
+        }  
+        //Serial.println("---Right Fog OFF---");
+      }
+
+      if ( LeftFog == true )
+      {
+        //Fog left enable
+        EnableTempFog = true;
+        digitalWrite(EnableFogLeft, HIGH); 
+        Serial.println("---Left Fog ON---"); 
+      }
+      else
+      {
+        //Fog left disable
+        EnableTempFog = false;
+        if (digitalRead(EnableFogLeft) != 0)
+        { 
+          digitalWrite(EnableFogLeft, LOW);
+          delay(5);
+        } 
+        //Serial.println("---Left Fog OFF---"); 
+      }   
     }
     else
     {
-      //Fog right disable
-      EnableTempFog = false;
-
-      if (digitalRead(EnableFogRight) != 0)
-      { 
-        digitalWrite(EnableFogRight, LOW);
-        delay(5);
-      }  
-      //Serial.println("---Right Fog OFF---");
-    }
-
-    if ( LeftFog == true )
-    {
-      //Fog left enable
-      EnableTempFog = true;
-      digitalWrite(EnableFogLeft, HIGH); 
-      Serial.println("---Left Fog ON---"); 
-    }
-    else
-    {
-      //Fog left disable
-      EnableTempFog = false;
-      if (digitalRead(EnableFogLeft) != 0)
-      { 
+      // Front Fog enable
+      if (EnableTempFog == true)
+      {
+        EnableTempFog = false;
+        Serial.println("---ALL Fog OFF---");
         digitalWrite(EnableFogLeft, LOW);
-        delay(5);
-      } 
-      //Serial.println("---Left Fog OFF---"); 
-    }   
-  }
-  else
-  {
-    // Front Fog enable
-    if (EnableTempFog == true)
-    {
-      EnableTempFog = false;
-      Serial.println("---ALL Fog OFF---");
-      digitalWrite(EnableFogLeft, LOW);
-      digitalWrite(EnableFogRight, LOW);
-    }
-  }   
+        digitalWrite(EnableFogRight, LOW);
+      }
+    } 
+  }  
 }
 
 void onCANReceive(int packetSize) 
@@ -274,12 +290,30 @@ void onCANReceive(int packetSize)
       else
       {
         Engine_Run = false;
-      }    
+      }
+      if (parameters[0] == 0x90)
+      {
+        Remote_start = true;
+        Engine_Run = true;
+      }
+      else
+      {
+        Remote_start = false;
+      }
+      Serial.print("0x");
+      Serial.print(packetID, HEX);
+      Serial.print(" Keystate: ");
+      Serial.print(packetSize);
+      for (uint8_t x = 0; x < packetSize; x++)
+      {
+        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+      }
+      Serial.println();
       break;
 
     case 0x015:
-      Jeep_Batt = parameters[1] / 10;
-      Jeep_Temp_Outdoor = parameters[0] / 10;
+      Jeep_Batt = parameters[1] / 10.0;
+      Jeep_Temp_Outdoor = parameters[0] / 10.0;
       
       if ( parameters[4] == 0x12 || parameters[4] == 0x13 ) //Left
       //if ( parameters[4] == 0x13 ) //Left
@@ -300,6 +334,15 @@ void onCANReceive(int packetSize)
       {
         RightFog = false; // Disable Right Fog Lamp
       }
+      Serial.print("0x");
+      Serial.print(packetID, HEX);
+      Serial.print(" Temp,Batt other: ");
+      Serial.print(packetSize);
+      for (uint8_t x = 0; x < packetSize; x++)
+      {
+        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+      }
+      Serial.println();
       break;
 
     case 0x006:
@@ -332,38 +375,119 @@ void onCANReceive(int packetSize)
       {
         Temp_Button_SW1 = 0;//сброс счетчика нажатий
       }
+      Serial.print("0x");
+      Serial.print(packetID, HEX);
+      Serial.print(" Keys: ");
+      Serial.print(packetSize);
+      for (uint8_t x = 0; x < packetSize; x++)
+      {
+        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+      }
+      Serial.println();
       break;
 
-    case 0x012:
+    case 0x012:      
       if (parameters[0] == 0x08 and (parameters[1] == 0x01 or parameters[1] == 0x02 or parameters[1] == 0x03 or parameters[1] == 0x04 or parameters[1] == 0x05))
       {
         reset_az_stage += 1;
         Serial.print("---Counter Remote Start Updated = ");Serial.print(reset_az_stage);Serial.print("----"); Serial.println();
         my_reset_az = millis();
       }
+      
       if ( parameters[0] == 0x05 ) //RKE key Trunk
       {
         RKE_Trunk_Button_flag = true; // Enable RKE key Trunk
         Serial.print("---Fobic Key Enabled Trunc----"); Serial.println();
       }
-      if ( parameters[0] == 0x01 or parameters[0] == 0x09 ) //RKE key Lock (Alarm ON)
+      if ( parameters[0] == 0x01 or parameters[0] == 0x09 ) //RKE key Alarm ON
       {
-        RKE_Alarm_ON_flag = true; // Enable RKE key Lock (Alarm ON)
-        Serial.print("---Fobic Key Enabled Lock Alarm ON----"); Serial.println();
+        RKE_Alarm_ON_flag = true; // Enable RKE key Alarm ON
+        Serial.print("---Fobic Key Enabled Alarm ON----"); Serial.println();
       }
-    break;
-
-    default:
-      //Output information from unexpected packets
-//      Serial.print("0x");
-//      Serial.print(packetID, HEX);
-//      Serial.print(" defaulted-ID size: ");
-//      Serial.print(packetSize);
+      Serial.print("0x");
+      Serial.print(packetID, HEX);
+      Serial.print(" RKE, IMMO, SKREEM: ");
+      Serial.print(packetSize);
       for (uint8_t x = 0; x < packetSize; x++)
       {
-//        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
       }
-//      Serial.println();
+      Serial.println();
+    break;
+
+    case 0x013:            
+      if ( parameters[2] == 0x00 ) //Alarm_Status
+      {
+        Alarm_Status = false; // Alarm_Status OFF
+        //Serial.print("---Alarm_Status OFF----"); Serial.println();
+      }
+      if ( parameters[2] == 0x01 or parameters[2] == 0x02 ) //Alarm_Status or Remote status
+      {
+        Alarm_Status = true; // Alarm_Status ON
+        //Serial.print("---Alarm_Status ON----"); Serial.println();
+      }
+      Serial.print("0x");
+      Serial.print(packetID, HEX);
+      Serial.print(" Status alarm, doors, other: ");
+      Serial.print(packetSize);
+      for (uint8_t x = 0; x < packetSize; x++)
+      {
+        Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+      }
+      Serial.println();
+    break;
+
+    case 0x1A2:
+      if (is_dodge == true)
+      {
+        Serial.println(" HVAC Found ID=0x1A2");
+      }
+      is_dodge = false;
+    break;
+
+    case 0x411:      
+      if (is_dodge == true)
+      {
+        Serial.println(" HVAC Found ID=0x411");
+      }
+      is_dodge = false;
+    break;
+
+    case 0x1AA:
+      Serial.print("---LRSM - Status: ");
+      Serial.print(parameters[0], HEX);
+      if (is_dodge == true)
+      {
+        if ( parameters[0] == 0x01 || parameters[0] == 0x21 || parameters[0] == 0x41 || parameters[0] == 0x61 ) //Check night
+        {
+          //Night 
+          Serial.print(" Night");
+          canSend(0x1A2, 0x00, 0xEF, 0x16, 0xFF, 0x00, 0x00); delay(5); 
+        }
+        if ( parameters[0] == 0x00 || parameters[0] == 0x20 || parameters[0] == 0x40 || parameters[0] == 0x60 ) //Check day
+        {
+          //Day
+          Serial.print(" Day");
+          canSend(0x1A2, 0x00, 0x6E, 0x16, 0xFF, 0x00, 0x00); delay(5); 
+        }
+      }
+      Serial.println(".---");
+    break;
+    
+    default:
+      if (CAN_LOGS == true)
+      {
+        //Output information from unexpected packets
+        Serial.print("0x");
+        Serial.print(packetID, HEX);
+        Serial.print(" defaulted-ID size: ");
+        Serial.print(packetSize);
+        for (uint8_t x = 0; x < packetSize; x++)
+        {
+          Serial.print(" 0x"); Serial.print(parameters[x], HEX);
+        }
+        Serial.println();
+      }
   }
 }
 
@@ -392,10 +516,9 @@ void Check_Counter_AZ()
 void reset_counter_az()
 {
   // сброс счетчика количества АЗ
-  // canId 11D, byte0=40 - Flash High Beams
   Serial.println("---Reset counter AZ----");
-  canSend(0x11D, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00);// Flash High Beam. Мигнем фарами
-  // to do function reset 
+
+  canSend(0x11D, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00);// Flash High Beam
   delay(5);
 }
 
@@ -404,44 +527,41 @@ void checkSerial()
   if (Serial.available())
   {
     char RX = Serial.read();
-    if (!SerialRXSpecial)
-    {
+      if ( RX == 'q' || RX == 'Q' ) //can logs on
+      {
+        CAN_LOGS = true;
+        Serial.print("---CAN LOGS ON--- ");Serial.println();
+      }
+      if ( RX == 'w' || RX == 'W' ) //can logs off
+      {
+        CAN_LOGS = false;
+        Serial.print("---CAN LOGS OFF--- ");Serial.println();
+      }
       if ( RX == 'R' || RX == 'r' ) //RKE
       {
         //keyState = 0x41;
-        //RKE_Trunk_Button_flag = true;
+        RKE_Trunk_Button_flag = true;
         //canSend(0x000, keyState, 0x00, 0x00, 0x00, 0x00, 0x00); delay(5);
       }
-      if ( RX == 'A' || RX == 'a' ) //AZ
+      if ( RX == 'A' || RX == 'a' ) //
       {
-        //keyState = 0x00;
+        // wake on can 
+        canSend(0x370, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00); delay(5);        
+      }
+      if ( RX == 'S' || RX == 's' ) //
+      {
+        // wake off can 
+        canSend(0x370, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00); delay(5);        
+      }
+      if ( RX == 'z' || RX == 'z' ) //AZ
+      {
+        //key_act = 0x08;
+        //key_id = 0x01;
+        keyState = 0x41;
+        Check_Counter_AZ();
+        keyState = 0x00;
         //canSend(0x000, keyState, 0x00, 0x00, 0x00, 0x00, 0x00); delay(5);       
       }
-      if ( RX == 'Z' || RX == 'z' ) //AZ
-      {
-        //keyState = 0x41;
-        //Check_Counter_AZ();
-        //keyState = 0x00;
-        //canSend(0x000, keyState, 0x00, 0x00, 0x00, 0x00, 0x00); delay(5);       
-      }
-      if ( RX == 'T' || RX == 't' )
-      {
-        SerialRXBuffer = RX;
-        SerialRXSpecial = true;
-      }
-    }
-    else
-    {
-      SerialRXBuffer += RX;
-      if (SerialRXBuffer.length() >= 5)
-      {
-        String tempVal = "";
-        char tempArray[8];
-        tempVal = SerialRXBuffer.substring(1,3);
-        SerialRXBuffer = "";
-        SerialRXSpecial = false;
-      }
-    }
   }
 }
 
